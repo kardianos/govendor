eabc765a1ae3b59184f37f3c7156b12e2308bf44
Daniel Theophanes <kardianos@gmail.com>
2015-04-16 13:27:49 -0700

    vendor: support using status to choose packages.

--- main.go
+++ main.go
@@ -6,64 +6,87 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/kardianos/vendor/rewrite"
-
-	kp "github.com/kardianos/vendor/internal/gopkg.in/alecthomas/kingpin.v1"
 )
 
-var (
-	initCmd = kp.Command("init", "create a new internal folder and vendor file")
+var help = `vendor: copy go packages locally and re-write imports.
+vendor init
+vendor list [status]
+vendor {add, update, remove} [-status] <import or status>
 
-	add       = kp.Command("add", "vendor a new package")
-	addImport = add.Arg("import", "import path").Required().String()
+Status list:
+	external - package does not share root path
+	internal - in vendor file; copied locally
+	unused - the package has been copied locally, but isn't used
+	local - shares the root path and is not a vendor package
+	missing - referenced but not found in GOROOT or GOPATH
+	std - standard library package
 
-	update       = kp.Command("update", "update an existing package")
-	updateImport = update.Arg("import", "import path").Required().String()
+Status can be referenced by their initial letters.
+	"st" == "std"
+	"e" == "external"
+	
+Example:
+	vendor add github.com/kardianos/osext
+	vendor add -status external
+	vendor update -status ext
+	vendor remove -status internal
+`
 
-	remove       = kp.Command("remove", "un-vendor existing package")
-	removeImport = remove.Arg("import", "import path").Required().String()
+func printHelpExit(errs ...error) {
+	for _, err := range errs {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	}
+	fmt.Fprint(os.Stderr, help)
+	os.Exit(1)
+}
 
-	list       = kp.Command("list", "list imports for packages")
-	listStatus = list.Arg("status", "[e]xternal, [i]nternal, [u]nused").String()
-)
+func parseStatus(s string) (status []rewrite.ListStatus, err error) {
+	switch {
+	case strings.HasPrefix("external", s):
+		status = []rewrite.ListStatus{rewrite.StatusExternal}
+	case strings.HasPrefix("internal", s):
+		status = []rewrite.ListStatus{rewrite.StatusInternal}
+	case strings.HasPrefix("unused", s):
+		status = []rewrite.ListStatus{rewrite.StatusUnused}
+	case strings.HasPrefix("missing", s):
+		status = []rewrite.ListStatus{rewrite.StatusMissing}
+	case strings.HasPrefix("local", s):
+		status = []rewrite.ListStatus{rewrite.StatusLocal}
+	case strings.HasPrefix("std", s):
+		status = []rewrite.ListStatus{rewrite.StatusStd}
+	default:
+		err = fmt.Errorf("unknown status %q", s)
+	}
+	return
+}
 
 func main() {
 	var err error
-	switch kp.Parse() {
+	if len(os.Args) == 1 {
+		printHelpExit()
+	}
+	cmd := os.Args[1]
+	switch cmd {
 	case "init":
 		err = rewrite.CmdInit()
-	case "add":
-		err = rewrite.CmdAdd(*addImport)
-	case "update":
-		err = rewrite.CmdUpdate(*updateImport)
-	case "remove":
-		err = rewrite.CmdRemove(*removeImport)
 	case "list":
-		var list []rewrite.ListItem
-		var status []rewrite.ListStatus
-		list, err = rewrite.CmdList()
-		switch {
-		case len(*listStatus) == 0:
-			status = []rewrite.ListStatus{rewrite.StatusExternal, rewrite.StatusInternal, rewrite.StatusUnused, rewrite.StatusMissing, rewrite.StatusLocal}
-		case strings.HasPrefix("external", *listStatus):
-			status = []rewrite.ListStatus{rewrite.StatusExternal}
-		case strings.HasPrefix("internal", *listStatus):
-			status = []rewrite.ListStatus{rewrite.StatusInternal}
-		case strings.HasPrefix("unused", *listStatus):
-			status = []rewrite.ListStatus{rewrite.StatusUnused}
-		case strings.HasPrefix("missing", *listStatus):
-			status = []rewrite.ListStatus{rewrite.StatusMissing}
-		case strings.HasPrefix("local", *listStatus):
-			status = []rewrite.ListStatus{rewrite.StatusLocal}
-		case strings.HasPrefix("std", *listStatus):
-			status = []rewrite.ListStatus{rewrite.StatusStd}
-		default:
-			kp.UsageErrorf("Unknown status to print: %s", *listStatus)
+		status := []rewrite.ListStatus{rewrite.StatusExternal, rewrite.StatusInternal, rewrite.StatusUnused, rewrite.StatusMissing, rewrite.StatusLocal}
+		// Parse status.
+		if len(os.Args) >= 3 {
+			status, err = parseStatus(os.Args[2])
+			if err != nil {
+				printHelpExit(err)
+			}
 		}
+		// Print all listed status.
+		var list []rewrite.ListItem
+		list, err = rewrite.CmdList()
 		for _, item := range list {
 			print := false
 			for _, s := range status {
@@ -76,8 +99,56 @@ func main() {
 				fmt.Println(item)
 			}
 		}
+	case "add", "update", "remove":
+		listFlags := flag.NewFlagSet("list", flag.ContinueOnError)
+		useStatus := listFlags.Bool("status", false, "")
+		err = listFlags.Parse(os.Args[2:])
+		if err != nil {
+			printHelpExit(err)
+		}
+		args := listFlags.Args()
+		if len(args) == 0 {
+			printHelpExit(fmt.Errorf("missing status"))
+		}
+		if *useStatus {
+			statusList, err := parseStatus(args[0])
+			if err != nil {
+				printHelpExit(err)
+			}
+			status := statusList[0]
+
+			list, err := rewrite.CmdList()
+			if err != nil {
+				printHelpExit(err)
+			}
+			for _, item := range list {
+				if item.Status != status {
+					continue
+				}
+
+				switch cmd {
+				case "add":
+					err = rewrite.CmdAdd(item.Path)
+				case "update":
+					err = rewrite.CmdUpdate(item.Path)
+				case "remove":
+					err = rewrite.CmdRemove(item.Path)
+				}
+			}
+		} else {
+			for _, arg := range args {
+				switch cmd {
+				case "add":
+					err = rewrite.CmdAdd(arg)
+				case "update":
+					err = rewrite.CmdUpdate(arg)
+				case "remove":
+					err = rewrite.CmdRemove(arg)
+				}
+			}
+		}
 	default:
-		kp.Usage()
+		printHelpExit()
 	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
