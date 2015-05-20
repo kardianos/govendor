62aed3cd637914bd8beb7bdc3ad54b9de283a1bf
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2015-05-20 09:11:05 -0400

    Add support for packages managed by Bazaar

--- rewrite/vcs.go
+++ rewrite/vcs.go
@@ -13,7 +13,7 @@ import (
 )
 
 type VcsInfo struct {
-	Dirty       bool
+	Dirty        bool
 	Revision     string
 	RevisionTime *time.Time
 }
@@ -26,6 +26,7 @@ type Vcs interface {
 var VcsRegistry = []Vcs{
 	VcsGit{},
 	VcsHg{},
+	VcsBzr{},
 }
 
 func FindVcs(root, packageDir string) (info *VcsInfo, err error) {
@@ -141,6 +142,53 @@ func (VcsHg) Find(dir string) (*VcsInfo, error) {
 			if err == nil {
 				info.RevisionTime = &tm
 			}
+		}
+	}
+	return info, nil
+}
+
+type VcsBzr struct{}
+
+func (VcsBzr) Find(dir string) (*VcsInfo, error) {
+	fi, err := os.Stat(filepath.Join(dir, ".bzr"))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	if fi.IsDir() == false {
+		return nil, nil
+	}
+
+	// Get info.
+	info := &VcsInfo{}
+
+	cmd := exec.Command("bzr", "status")
+	cmd.Dir = dir
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	if string(output) != "" {
+		info.Dirty = true
+	}
+
+	cmd = exec.Command("bzr", "log", "-r-1")
+	cmd.Dir = dir
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	for _, line := range strings.Split(string(output), "\n") {
+		if strings.HasPrefix(line, "revno:") {
+			info.Revision = strings.Split(strings.TrimSpace(strings.TrimPrefix(line, "revno:")), " ")[0]
+		} else if strings.HasPrefix(line, "timestamp:") {
+			tm, err := time.Parse("Mon 2006-01-02 15:04:05 -0700", strings.TrimSpace(strings.TrimPrefix(line, "timestamp:")))
+			if err != nil {
+				return nil, err
+			}
+			info.RevisionTime = &tm
 		}
 	}
 	return info, nil
